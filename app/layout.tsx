1be29028d90ceec9ac4fc1d354f97b1e9c6cc237
grinzing <grinzing35@gmail.com>
2025-04-25 13:29:36 +0900

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type React from "react"
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="ja" suppressHydrationWarning>
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Shippori+Mincho:wght@400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem disableTransitionOnChange>
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
+
+export const metadata = {
+      generator: 'v0.dev'
+    };
